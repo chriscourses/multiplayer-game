88d18d89ada36cd88dee74a1671176d14903eb67
Christopher Lis <christopher4lis@gmail.com>
2023-04-27 16:38:52 -0400

    Connect and disconnect players

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+
+// socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
+
 const port = 3000
 
 app.use(express.static('public'))
@@ -8,6 +15,28 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+const players = {}
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  players[socket.id] = {
+    x: 500 * Math.random(),
+    y: 500 * Math.random()
+  }
+
+  io.emit('updatePlayers', players)
+
+  socket.on('disconnect', (reason) => {
+    console.log(reason)
+    delete players[socket.id]
+    io.emit('updatePlayers', players)
+  })
+
+  console.log(players)
+})
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+console.log('server did load')
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-This is a starter project for the [Chris Courses Online Multiplayer Game Tutorial](https://www.youtube.com/watch?v=Wcvqnx14cZA) available on YouTube.
+This is the official repo for the [Chris Courses Online Multiplayer Game Tutorial](https://www.youtube.com/watch?v=Wcvqnx14cZA) available on YouTube.
 
-To get started, download this repo and double click `index.html` to see the single-player game we'll base our multiplayer-game off of.
+You can view each episode's progress by selecting this repo's commits individually.
